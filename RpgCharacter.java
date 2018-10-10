0c64a3878e1f0ded24e520e5664ead4cc2767c6b
OkaGouhei <okagouhei@okagoutairanoMacBook-puro.local>
2018-10-10 20:29:12 +0900

    Java7の課題
    mainメソッドをCharacterクラスではなくて別のクラスに移動した
    Character型の配列変数を用意してパーティーを組めるようにした
    パーティーを２つ用意して全員にアタックメソッドを実行させる（攻撃対象はランダム）

--- RpgCharacter.java
+++ RpgCharacter.java
@@ -1,12 +1,31 @@
+    import java.util.Random;
 class RpgCharacter{
   public static void main(String[] args){
-    Character a = new Character("大王",100,200);
-    a.introduce();
-    Character b = new Character("魔王",300,400);
-    b.introduce();
-    b.attack(a);
-    a.introduce();
-    a.attack(b);
-    b.introduce();
+    // Character a = new Character("大王",100,200);
+    // a.introduce();
+    // Character b = new Character("魔王",300,400);
+    // b.introduce();
+    Character[] party_daiou = new Character[4];
+    party_daiou[0] = new Character("大王",80,20);
+    party_daiou[1] = new Character("大大王",50,40);
+    party_daiou[2] = new Character("中大王",60,30);
+    party_daiou[3] = new Character("小大王",80,20);
+    Character[] party_maou = new Character[4];
+    party_maou[0] = new Character("魔王王",80,20);
+    party_maou[1] = new Character("大魔王",50,40);
+    party_maou[2] = new Character("中魔王",60,30);
+    party_maou[3] = new Character("小魔王",80,20);
+    // party_maou[1].attack(party_daiou[1]);
+    // party_daiou[1].introduce();
+    Random r = new Random();
+    int num_daiou = r.nextInt(4);
+    int num_maou = r.nextInt(4);
+    party_maou[1].attack(party_daiou[num_daiou]);
+    party_daiou[num_daiou].introduce();
+    party_daiou[1].attack(party_maou[num_maou]);
+    party_maou[num_maou].introduce();
+
+    // a.attack(b);
+    // b.introduce();
   }
 }
