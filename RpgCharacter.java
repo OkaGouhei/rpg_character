ea3955746bf314e39a58b30860b47e7dbc9eb738
OkaGouhei <okagouhei@okagoutairanoMacBook-puro.local>
2018-10-14 10:02:20 +0900

    修正
    ・RpgCharacter1行目のインデントがずれている
    
    ・使用しないコードはコメントアウトではなく削除。

--- RpgCharacter.java
+++ RpgCharacter.java
@@ -1,10 +1,6 @@
-    import java.util.Random;
+import java.util.Random;
 class RpgCharacter{
   public static void main(String[] args){
-    // Character a = new Character("大王",100,200);
-    // a.introduce();
-    // Character b = new Character("魔王",300,400);
-    // b.introduce();
     Character[] party_daiou = new Character[4];
     party_daiou[0] = new Character("大王",80,20);
     party_daiou[1] = new Character("大大王",50,40);
@@ -15,8 +11,6 @@ class RpgCharacter{
     party_maou[1] = new Character("大魔王",50,40);
     party_maou[2] = new Character("中魔王",60,30);
     party_maou[3] = new Character("小魔王",80,20);
-    // party_maou[1].attack(party_daiou[1]);
-    // party_daiou[1].introduce();
     Random r = new Random();
     while(true){
       for (int i=0; i<4;i++){
@@ -36,7 +30,5 @@ class RpgCharacter{
         }
       }
     }
-    // a.attack(b);
-    // b.introduce();
   }
 }
